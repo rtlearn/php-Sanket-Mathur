3ccc43142e2bab2b5dd2443dd4e17e833ecff106
Sanket-Mathur <rajeev.sanket@gmail.com>
2021-12-24 02:37:08 +0530

    add: Initial design for the webpage

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>rtCamp Challenge - Email a random XKCD challenge</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div class="container">
+        <h1>Random XKCD Comics</h1>
+        <p>You will receive comics every 5 minutes on your email after subscribing.</p>
+        <form action="" method="post">
+            <label>Email Address: </label>
+            <input type="email" name="email" size="50">
+            <br>
+            <button type="submit" name="submit">Send Verificaton Code</button>
+        </form>
+    </div>
+</body>
+</html>
